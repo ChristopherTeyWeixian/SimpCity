9c42878830e3b676a6ec9b67bdaa7e119544097e
Creldra <S10189894@connect.np.edu.sg>
2022-01-01 22:18:09 +0800

    Completed Load Saved Game #12

--- Main.py
+++ Main.py
@@ -28,7 +28,24 @@ while (True):
                         break
 
         case "2":#Load saved game
-            print("you chose 2")
+            board = Board()
+            if board.Load_Board() == False:
+                continue
+            while(True):
+                game_opt,building_to_build = game_menu(board)
+                match game_opt:
+                    case "1":
+                        place_building(board,building_to_build)
+                    case "2":
+                        place_building(board,building_to_build)
+                    case "3":
+                        display_remaining_building(board)
+                    case "4":
+                        print("see current score, not yet implement")
+                    case "5":
+                        board.Save_Board()
+                    case "0":
+                        break
 
         case "0":#Exit
             quit()
--- files/Board.py
+++ files/Board.py
@@ -45,7 +45,37 @@ class Board(object):
             return False
 
     def Load_Board(self):
-        return
+        try:
+            # adds_line is needed as "\b" is counted as a functionality in python
+            # thus a roundabout way was used instead of just changing the file name
+            adds_line = "\q"
+            adds_line = adds_line.replace("q", "")
+
+            # current file path not+ \..\data\baseboard.xlxs
+            # python files is different location as data files
+            path = str(pathlib.Path(__file__).parent.resolve()) + "\.." + adds_line + "data\save_board.xlsx"
+            #print(path)
+
+            # loads the excel file of path
+            wb_obj = openpyxl.load_workbook(path)
+            sheet_obj = wb_obj.active
+
+            # nested for loop to get each cell one by one and append into the board
+            for x in range(1, sheet_obj.max_row + 1):
+                temp_list = []
+                for y in range(1, sheet_obj.max_column + 1):
+                    cell_obj = sheet_obj.cell(row = x, column = y).value
+                    if cell_obj == "e":
+                        cell_obj = cell_obj.replace("e", " ")
+                    elif cell_obj == "n":
+                        cell_obj = cell_obj.replace("n", "")
+                    temp_list.append(cell_obj)
+                self.board.append(temp_list)
+
+            return True
+        except:
+            print("[!] Could not connect to Excel Database, Please Try Again.\n")
+            return False
 
     def Save_Board(self):
         try:
