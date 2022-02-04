8899f2a6b21ad8ed25bc51f566af48b2d964242f
Pek Jun Teck Keith <keith_pek@yahoo.com.sg>
2022-02-04 21:34:50 +0800

    Modifications done for check_adjacent, as well as functions linked to check_adjacent regarding coords and certain returns

--- Main.py
+++ Main.py
@@ -37,21 +37,26 @@ while (True):
             if board.Load_Board() == False:
                 continue
             while(True):
-                game_opt,building_to_build = game_menu(board)
-                match game_opt:
-                    case "1":
-                        choose = str(input("Build where? "))
-                        place_building(board, building_to_build, choose)
-                    case "2":
-                        choose = str(input("Build where? "))
-                        place_building(board, building_to_build, choose)
-                    case "3":
-                        CalculateScore(board)
-                        print(board.score)
-                    case "4":
-                        board.Save_Board()
-                    case "0":
-                        break
+                if board.turn >= 17:
+                    EndGame(board)
+                    CalculateScore(board)
+                    break
+                else:
+                    game_opt,building_to_build = game_menu(board)
+                    match game_opt:
+                        case "1":
+                            choose = str(input("Build where? "))
+                            place_building(board, building_to_build, choose)
+                        case "2":
+                            choose = str(input("Build where? "))
+                            place_building(board, building_to_build, choose)
+                        case "3":
+                            CalculateScore(board)
+                            print(board.score)
+                        case "4":
+                            board.Save_Board()
+                        case "0":
+                            break
         case "3":
             board = Board()
             board.Get_BuildingState()
